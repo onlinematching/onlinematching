9ceb659bf9c786fba1fa78a5d6b04da3cdb4b412
shinnku <shinnkunikaidou@gmail.com>
2023-03-27 16:12:21 +0800

    .

--- tests/test_bigraph.rs
+++ tests/test_bigraph.rs
@@ -15,22 +15,23 @@ mod tests_bigraph {
         ]);
         println!("{:?}", g);
         g.insert_v("v2")
-            .expect("The online nodes already have this key");
+            .expect_err("The online nodes already have this key");
         drop(g);
     }
-
 
     #[test]
     fn weighted_bigraph_test() {
         let mut g = WBigraph::from_edges(&vec![
-            (("u1", "v1"),1),
-            (("u2", "v1"),2),
-            (("u3", "v1"),3),
-            (("u2", "v2"),4),
+            (("u1", "v1"), 1),
+            (("u2", "v1"), 2),
+            (("u3", "v1"), 3),
+            (("u2", "v2"), 4),
         ]);
         println!("{:?}", g);
-        g.insert_v("v2")
+        g.insert_v("v3")
             .expect("The online nodes already have this key");
+
+        println!("{:?}", g);
         drop(g);
     }
 }
